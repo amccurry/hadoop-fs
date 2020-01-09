7cd7091a88632e8968ca5a8acc32e8a64e28279e
Aaron McCurry <amccurry@gmail.com>
2020-01-09 15:44:41 -0500

    Starting on a cache fs.

---
+++ src/test/java/hadoop/fs/cache/CacheFileSystemTest.java
@@ -0,0 +1,129 @@
+package hadoop.fs.cache;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.UUID;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocalFileSystem;
+import org.apache.hadoop.fs.Path;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CacheFileSystemTest {
+
+  private static final String CACHE_TEST_FS = "cache.test.fs";
+  private File ROOT = new File("./target/tmp/" + getClass().getName());
+  private Configuration _conf;
+  private Path _cacheFsRoot;
+  private Path _realPath;
+
+  @Before
+  public void setup() throws Exception {
+    FileSystem.closeAll();
+    String rootPathStr = ROOT.getCanonicalPath();
+    _conf = new Configuration();
+    LocalFileSystem local = FileSystem.getLocal(_conf);
+    Path rootPath = new Path(rootPathStr);
+    local.delete(rootPath, true);
+    local.mkdirs(rootPath);
+    rootPath = local.makeQualified(rootPath);
+
+    _realPath = new Path(rootPath, "real");
+    local.mkdirs(_realPath);
+    _realPath = local.makeQualified(_realPath);
+    _cacheFsRoot = new Path("cache", "test", _realPath.toUri()
+                                                      .getPath());
+    FileSystem fileSystem = _realPath.getFileSystem(_conf);
+    fileSystem.mkdirs(_realPath);
+
+    _conf.set(CACHE_TEST_FS, _realPath.toString());
+    assertEmptyDir(_cacheFsRoot);
+  }
+
+  @Test
+  public void testFileStatusWhileWriting() throws IOException {
+    Path file = new Path(_cacheFsRoot, UUID.randomUUID()
+                                           .toString());
+
+    FileSystem fileSystem = file.getFileSystem(_conf);
+
+    assertFalse(fileSystem.exists(file));
+
+    try (FSDataOutputStream output = fileSystem.create(file)) {
+      assertTrue(fileSystem.exists(file));
+    }
+
+    assertTrue(fileSystem.exists(file));
+    assertFileExists(file);
+    assertFileExists(new Path(_realPath, file.getName()));
+
+  }
+
+  @Test
+  public void testCRDFile() throws IOException {
+    Path file = new Path(_cacheFsRoot, UUID.randomUUID()
+                                           .toString());
+    touchFile(file);
+    assertFileExists(file);
+    assertFileExists(new Path(_realPath, file.getName()));
+  }
+
+  @Test
+  public void testFileStatus() throws IOException {
+    Path file = new Path(_cacheFsRoot, UUID.randomUUID()
+                                           .toString());
+    touchFile(file);
+    assertFileExists(file);
+
+    FileSystem fileSystem = file.getFileSystem(_conf);
+    FileStatus[] listStatus = fileSystem.listStatus(file);
+    assertEquals(1, listStatus.length);
+    assertEquals(file, listStatus[0].getPath());
+  }
+
+  @Test
+  public void testFileNotFoundError() throws IOException {
+    Path file = new Path(_cacheFsRoot, UUID.randomUUID()
+                                           .toString());
+    FileSystem fileSystem = file.getFileSystem(_conf);
+    try {
+      fileSystem.getFileStatus(file);
+      fail();
+    } catch (Exception e) {
+      if (e instanceof FileNotFoundException) {
+
+      } else {
+        fail();
+      }
+    }
+  }
+
+  private void assertFileExists(Path path) throws IOException {
+    FileStatus fileStatus = path.getFileSystem(_conf)
+                                .getFileStatus(path);
+    assertTrue(fileStatus.isFile());
+  }
+
+  private void touchFile(Path path) throws IOException {
+    path.getFileSystem(_conf)
+        .create(path)
+        .close();
+  }
+
+  private void assertEmptyDir(Path path) throws IOException {
+    FileSystem fileSystem = path.getFileSystem(_conf);
+    assertTrue(fileSystem.exists(path));
+    assertEquals(0, fileSystem.listStatus(path).length);
+  }
+
+}
